4f33d531e7bd3ae3b85fb8884850403cc4c77cfd
ajaygupta1999 <2018.ajay.gupta@ves.ac.in>
2022-04-26 11:38:46 +0530

    small rounting changes are done

--- pipelinetriggers/trigger.sh
+++ pipelinetriggers/trigger.sh
@@ -2,9 +2,10 @@
 
 # HMAC generated from payload + GH secret
 
-curl -i \
+curl -v \
+  -H 'Host: 4d06-206-84-239-100.in.ngrok.io' \
   -H 'X-GitHub-Event: push' \
-  -H 'X-Hub-Signature: sha1=<...>' \
+  -H 'X-Hub-Signature: sha1=59a349188894fc5e7d7030ca2190e604542e4c1f' \
   -H 'Content-Type: application/json' \
-  -d '{"ref":"refs/heads/tekton-testing","head_commit":{"id":"d9c7ccb0713da882d8373169e2a1d705f5959386"}}' \
-  http://tekton-triggers.daschner.dev
+  -d '{"ref":"refs/heads/master","head_commit":{"id":"99066040af712e102390354d8cb4b2fca18ab9e7"}}' \
+  https://192.168.39.229
